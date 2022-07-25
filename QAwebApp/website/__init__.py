598d1ac42e1f3b65ffa26f6d0bbbc4c82087991f
DanielTuckwood <37795246+DanielTuckwood@users.noreply.github.com>
2022-07-25 20:08:06 +0100

    Add files via upload

--- QAwebApp/website/__init__.py
+++ QAwebApp/website/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
+#Initiate Database
 db = SQLAlchemy()
-DB_NAME = "appDatabase.db"
+DB_NAME = "database.db"
 
+#Create app function
 def create_app():
+    from .models import User, Case, Note
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secretKey'
-    app.config['SQLAlCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = 'secretkey123'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///QAappDB'
     db.init_app(app)
 
     from .views import views
@@ -16,4 +21,20 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+#Create database function
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
--- QAwebApp/website/models.py
+++ QAwebApp/website/models.py
@@ -1,16 +1,31 @@
 from enum import unique
-from . import db
 from flask_login import UserMixin
+from datetime import datetime
+from . import db
 
+#Case table model
 class Case(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     sponsorURN = db.Column(db.String(150))
     case_type = db.Column(db.String(150))
+    txt = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=datetime.now)
 
+#Note table model
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    txt = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=datetime.now)
 
+#User table model
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    admin = db.Column(db.Boolean)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    cases = db.relationship('Case')
+    notes = db.relationship('Note')
 
